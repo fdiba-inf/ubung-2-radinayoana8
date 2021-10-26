fd14b3fe57d483ce652145d4933a543efeb6f87f
radinayoana8 <>
2021-10-26 16:30:05 +0000

    Files updated

--- src/exercise2/EvenOrOddNumber.java
+++ src/exercise2/EvenOrOddNumber.java
@@ -7,7 +7,7 @@ public class EvenOrOddNumber {
    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-      System.out.print("Enter a number: ");
+      System.out.print("Enter a number");
       int number = input.nextInt();
         
       if ( number % 2 == 0){
--- src/exercise2/QuadraticEquation.java
+++ src/exercise2/QuadraticEquation.java
@@ -1,39 +1,48 @@
 package exercise2;
-
 import java.util.Scanner;
+import java.lang.Math;
 
-public class QuadraticEquation {
+public class QuadraticEquation{
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    float a = input.nextFloat();
+     float b = input.nextFloat();
+    float c = input.nextFloat();
+    double D = b*b - (4.0*a*c);
+    double x1;
+     double x2;
+    float x ;
 
-   public static void main(String[] args) {
-     Scanner input = new Scanner(System.in); 
-
-     System.out.print("Enter a , b, c: ");
-     double a = input.nextDouble();
-     double b = input.nextDouble();
-     double c = input.nextDouble();
-
-     if (a != 0) {
-       double d = b * b - 4 * a * c;
-       if (d >= 0) {
-         double x1 = (-1 + Math.sqrt(d)) / 2 * a;
-         double x2 = (-1 - Math.sqrt(d)) / 2 * a;
-         System.out.println(x1 + "," + x2);
-      } else {
-        System.out.println( "Imaginary values");
+    if (a !=0){
+      if(D >= 0){
+        x1 = (b*-1 + Math.sqrt(D)) / (2*a);
+        x2 = (b*(-1) - Math.sqrt(D)) / (2*a);
+          System.out.print(x1);
+        System.out.print(", ");
+        System.out.print(x2);
+         }else{
+        System.out.println("Imaginary values");
       }
-   
-     }  else if (b != 0) {
-         double x = -c / b;
-        System.out.println(x);
-     } else if (c != 0) {
-        System.out.println("No values");
-     } else {
-        System.out.println("Many values");
+    }else{
+     if (b !=0){
+       x = (0-c) / b;
+       System.out.println(x);
+       
+     }else{
+       if(c !=0){
+          System.out.println("No values");
+       }else{
+         System.out.println("Many values");
+       }
      }
+    }
 
   }
+}
+    
 
- }     
+ 
+   
 
 
    
